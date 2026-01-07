11cbdb3412a6d306152a107f6cf721fa66ea8e58
z3rokwq <z3rokwq@gmail.com>
2026-01-07 12:31:19 +0300

    ims: Import IMS stack
    Signed-off-by: z3rokwq <z3rokwq@gmail.com>

--- blobs/pissarro-ims-vendor.mk
+++ blobs/pissarro-ims-vendor.mk
@@ -4,3 +4,15 @@
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/xiaomi/pissarro-ims/blobs
+
+PRODUCT_PACKAGES += \
+    ImsService \
+    MtkGbaService \
+    mediatek-common \
+    mediatek-framework \
+    mediatek-ims-base \
+    mediatek-ims-common \
+    mediatek-ims-extension-plugin \
+    mediatek-telecom-common \
+    mediatek-telephony-base \
+    mediatek-telephony-common
--- ims.mk
+++ ims.mk
@@ -5,3 +5,19 @@
 
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/pissarro-ims/blobs/pissarro-ims-vendor.mk)
+
+# IMS (App)
+PRODUCT_PACKAGES += \
+    ImsService \
+    MtkGbaService
+
+# IMS (Jars)
+PRODUCT_BOOT_JARS += \
+    mediatek-common \
+    mediatek-framework \
+    mediatek-ims-base \
+    mediatek-ims-common \
+	mediatek-ims-extension-plugin \
+    mediatek-telecom-common \
+    mediatek-telephony-base \
+    mediatek-telephony-common
