a744e57488ffdad1966179f859dbf00cae0ca5a7
z3rokwq <z3rokwq@gmail.com>
2026-09-14 16:47:41 +0300

    ims: Import IMS Videotelephony stack
    Signed-off-by: z3rokwq <z3rokwq@gmail.com>

--- blobs/pissarro-ims-vendor.mk
+++ blobs/pissarro-ims-vendor.mk
@@ -5,7 +5,27 @@
 PRODUCT_SOONG_NAMESPACES += \
     vendor/xiaomi/pissarro-ims/blobs
 
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/pissarro-ims/blobs/proprietary/system/etc/init/init.vtservice.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/init.vtservice.rc \
+    vendor/xiaomi/pissarro-ims/blobs/proprietary/vendor/etc/init/init.vtservice_hidl.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.vtservice_hidl.rc
+
 PRODUCT_PACKAGES += \
+    libcomutils \
+    libimsma \
+    libimsma_adapt \
+    libimsma_rtp \
+    libimsma_socketwrapper \
+    libmtk_vt_service \
+    libmtk_vt_wrapper \
+    libsignal \
+    libsink \
+    libsource \
+    libvcodec_cap \
+    libvcodec_capenc \
+    libvt_avsync \
+    vendor.mediatek.hardware.videotelephony@1.0 \
+    vendor.mediatek.hardware.videotelephony@1.0-impl \
+    vendor.mediatek.hardware.videotelephony@1.0 \
     ImsService \
     MtkGbaService \
     mediatek-common \
@@ -15,4 +35,6 @@ PRODUCT_PACKAGES += \
     mediatek-ims-extension-plugin \
     mediatek-telecom-common \
     mediatek-telephony-base \
-    mediatek-telephony-common
+    mediatek-telephony-common \
+    vtservice \
+    vtservice_hidl
